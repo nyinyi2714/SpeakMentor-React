f1f0ddbfb75abc3ed497b76787e68b9ad0b1e17a
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2024-02-20 13:18:05 -0800

    init commit

--- src/config.jsx
+++ src/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-  backendUrl: "http://18.119.99.102",
+  backendUrl: "http://127.0.0.1:8000",
   dictionaryAPI: "https://api.dictionaryapi.dev/api/v2/entries/en/",
   speechSuperAppKey: process.env.REACT_APP_SPEECHSUPER_API_KEY,
   speechSuperSecretKey: process.env.REACT_APP_SPEECHSUPER_SECRET_KEY,
