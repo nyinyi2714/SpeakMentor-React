708a751bbb77d5c33a78e3a8681476addb98b5e6
Nyi Nyi Moe Htet <68096773+nyinyi2714@users.noreply.github.com>
2024-04-10 01:56:57 -0700

    integrate gsap library

--- src/pages/PaymentComplete/PaymentComplete.jsx
+++ src/pages/PaymentComplete/PaymentComplete.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Navbar } from "../../components";
+import { gsap } from "gsap";
 import "./PaymentComplete.css"
 
 function PaymentComplete() {
+  const messageRef = useRef(null);
+
+  useEffect(() => {
+    gsap.from(messageRef.current.children, {
+      opacity: 0,
+      y: 30,
+      duration: 0.7,
+      delay: 0.2,
+      stagger: 0.12,
+    });
+
+  }, [])
+
   return (
     <>
       <Navbar />
       <div className="payment-complete-page">
-        <div className="message">
+        <div ref={messageRef} className="message">
           <img src="images/payment-complete.png" alt="thank-you-note" />
           <h1>Thank you for your subscription!</h1>
           <p>Your payment is complete, and you're ready to unlock your full pronunciation potential.</p>
